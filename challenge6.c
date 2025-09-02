49de4c941e20314dd614fa55f1ee152811eb8998
Wassim-RF <wassimrifi0313@gmail.com>
2025-09-02 15:41:30 +0200

    challenge 6

--- challenge6.c
+++ challenge6.c
@@ -2,5 +2,23 @@
 
 int main() {
     int num1 , num2 ;
-    
+    int add , sub , mult ;
+    float div;
+
+    printf("Entre le premiere nombre : ");
+    scanf("%d" , &num1);
+    printf("Entrer le deuxieme nombre : ");
+    scanf("%d" , &num2);
+    add = num1 + num2;
+    sub = num1 - num2;
+    mult = num1 * num2;
+    div = num1/num2 ;
+
+    printf("%d + %d = %d\n" , num1 , num2 , add);
+    printf("%d - %d = %d\n" , num1 , num2 , sub);
+    printf("%d * %d = %d\n" , num1 , num2 , mult);
+    printf("%d / %d = %.2f\n" , num1 , num2 , div);
+
+    return 0;
+
 }
